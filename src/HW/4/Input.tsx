7694f8d794d9f93d6092e48fd8de290cd6afaddf
Tatiana Kedrova <Baletrot@gmail.com>
2026-06-02 09:38:13 +0300

    corrected HW4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,19 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  currentText: string;
+  setCurrentText: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+export const Input = ({ currentText, setCurrentText }: InputPropsType) => {
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentText(event.currentTarget.value);
+  };
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+  return (
+    <input
+      id={"hw04-input"}
+      type="text"
+      value={currentText}
+      onChange={onChangeHandler}
+    />
+  );
 };
